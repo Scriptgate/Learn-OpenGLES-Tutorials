e20449311de5bb134fdfd3f1ccec2448b857be8e
Scriptgate <wscheppers@hotmail.com>
2016-11-05 18:01:00 +0100

    Created builder class for creating triangle vertices

---
+++ android/AndroidOpenGLESLessons/app/src/main/java/com/learnopengles/android/lesson1/Color.java
@@ -0,0 +1,28 @@
+package com.learnopengles.android.lesson1;
+
+public class Color {
+
+    public static final Color RED = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+    public static final Color GREEN = new Color(0.0f, 1.0f, 0.0f, 1.0f);
+    public static final Color BLUE = new Color(0.0f, 0.0f, 1.0f, 1.0f);
+    public static final Color WHITE = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+    public static final Color GREY = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+    public static final Color BLACK = new Color(0.0f, 0.0f, 0.0f, 1.0f);
+    public static final Color MAGENTA = new Color(1.0f, 0.0f, 1.0f, 1.0f);
+    public static final Color YELLOW = new Color(1.0f, 1.0f, 0.0f, 1.0f);
+    public static final Color CYAN = new Color(0.0f, 1.0f, 1.0f, 1.0f);
+
+
+    public final float red;
+    public final float green;
+    public final float blue;
+    public final float alpha;
+
+
+    public Color(float red, float green, float blue, float alpha) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.alpha = alpha;
+    }
+}
---
+++ android/AndroidOpenGLESLessons/app/src/main/java/com/learnopengles/android/lesson1/ColorPoint.java
@@ -0,0 +1,12 @@
+package com.learnopengles.android.lesson1;
+
+public class ColorPoint {
+
+    public final Point point;
+    public final Color color;
+
+    public ColorPoint(Point point, Color color) {
+        this.point = point;
+        this.color = color;
+    }
+}
