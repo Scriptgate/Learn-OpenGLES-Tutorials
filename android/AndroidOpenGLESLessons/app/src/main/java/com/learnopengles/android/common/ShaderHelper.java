1f7f88e1ace7febe722677e50cfa86fe1ae323bf
Scriptgate <wscheppers@hotmail.com>
2016-11-06 23:49:27 +0100

    Formatting

--- android/AndroidOpenGLESLessons/app/src/main/java/com/learnopengles/android/common/ShaderHelper.java
+++ android/AndroidOpenGLESLessons/app/src/main/java/com/learnopengles/android/common/ShaderHelper.java
@@ -1,103 +1,93 @@
 package com.learnopengles.android.common;
 
 import static android.opengl.GLES20.*;
+
 import android.util.Log;
 
-public class ShaderHelper
-{
-	private static final String TAG = "ShaderHelper";
+public class ShaderHelper {
+    private static final String TAG = "ShaderHelper";
 
-	/**
-	 * Helper function to compile a shader.
-	 *
-	 * @param shaderType The shader type.
-	 * @param shaderSource The shader source code.
-	 * @return An OpenGL handle to the shader.
-	 */
-	public static int compileShader(final int shaderType, final String shaderSource)
-	{
-		int shaderHandle = glCreateShader(shaderType);
+    /**
+     * Helper function to compile a shader.
+     *
+     * @param shaderType   The shader type.
+     * @param shaderSource The shader source code.
+     * @return An OpenGL handle to the shader.
+     */
+    public static int compileShader(final int shaderType, final String shaderSource) {
+        int shaderHandle = glCreateShader(shaderType);
 
-		if (shaderHandle != 0)
-		{
-			// Pass in the shader source.
-			glShaderSource(shaderHandle, shaderSource);
+        if (shaderHandle != 0) {
+            // Pass in the shader source.
+            glShaderSource(shaderHandle, shaderSource);
 
-			// Compile the shader.
-			glCompileShader(shaderHandle);
+            // Compile the shader.
+            glCompileShader(shaderHandle);
 
-			// Get the compilation status.
-			final int[] compileStatus = new int[1];
-			glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, compileStatus, 0);
+            // Get the compilation status.
+            final int[] compileStatus = new int[1];
+            glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, compileStatus, 0);
 
-			// If the compilation failed, delete the shader.
-			if (compileStatus[0] == 0)
-			{
-				Log.e(TAG, "Error compiling shader: " + glGetShaderInfoLog(shaderHandle));
-				glDeleteShader(shaderHandle);
-				shaderHandle = 0;
-			}
-		}
+            // If the compilation failed, delete the shader.
+            if (compileStatus[0] == 0) {
+                Log.e(TAG, "Error compiling shader: " + glGetShaderInfoLog(shaderHandle));
+                glDeleteShader(shaderHandle);
+                shaderHandle = 0;
+            }
+        }
 
-		if (shaderHandle == 0)
-		{
-			throw new RuntimeException("Error creating shader.");
-		}
+        if (shaderHandle == 0) {
+            throw new RuntimeException("Error creating shader.");
+        }
 
-		return shaderHandle;
-	}
+        return shaderHandle;
+    }
 
-	/**
-	 * Helper function to compile and link a program.
-	 *
-	 * @param vertexShaderHandle An OpenGL handle to an already-compiled vertex shader.
-	 * @param fragmentShaderHandle An OpenGL handle to an already-compiled fragment shader.
-	 * @param attributes Attributes that need to be bound to the program.
-	 * @return An OpenGL handle to the program.
-	 */
-	public static int createAndLinkProgram(final int vertexShaderHandle, final int fragmentShaderHandle, final String[] attributes)
-	{
-		int programHandle = glCreateProgram();
+    /**
+     * Helper function to compile and link a program.
+     *
+     * @param vertexShaderHandle   An OpenGL handle to an already-compiled vertex shader.
+     * @param fragmentShaderHandle An OpenGL handle to an already-compiled fragment shader.
+     * @param attributes           Attributes that need to be bound to the program.
+     * @return An OpenGL handle to the program.
+     */
+    public static int createAndLinkProgram(final int vertexShaderHandle, final int fragmentShaderHandle, final String[] attributes) {
+        int programHandle = glCreateProgram();
 
-		if (programHandle != 0)
-		{
-			// Bind the vertex shader to the program.
-			glAttachShader(programHandle, vertexShaderHandle);
+        if (programHandle != 0) {
+            // Bind the vertex shader to the program.
+            glAttachShader(programHandle, vertexShaderHandle);
 
-			// Bind the fragment shader to the program.
-			glAttachShader(programHandle, fragmentShaderHandle);
+            // Bind the fragment shader to the program.
+            glAttachShader(programHandle, fragmentShaderHandle);
 
-			// Bind attributes
-			if (attributes != null)
-			{
-				final int size = attributes.length;
-				for (int i = 0; i < size; i++)
-				{
-					glBindAttribLocation(programHandle, i, attributes[i]);
-				}
-			}
+            // Bind attributes
+            if (attributes != null) {
+                final int size = attributes.length;
+                for (int i = 0; i < size; i++) {
+                    glBindAttribLocation(programHandle, i, attributes[i]);
+                }
+            }
 
-			// Link the two shaders together into a program.
-			glLinkProgram(programHandle);
+            // Link the two shaders together into a program.
+            glLinkProgram(programHandle);
 
-			// Get the link status.
-			final int[] linkStatus = new int[1];
-			glGetProgramiv(programHandle, GL_LINK_STATUS, linkStatus, 0);
+            // Get the link status.
+            final int[] linkStatus = new int[1];
+            glGetProgramiv(programHandle, GL_LINK_STATUS, linkStatus, 0);
 
-			// If the link failed, delete the program.
-			if (linkStatus[0] == 0)
-			{
-				Log.e(TAG, "Error compiling program: " + glGetProgramInfoLog(programHandle));
-				glDeleteProgram(programHandle);
-				programHandle = 0;
-			}
-		}
+            // If the link failed, delete the program.
+            if (linkStatus[0] == 0) {
+                Log.e(TAG, "Error compiling program: " + glGetProgramInfoLog(programHandle));
+                glDeleteProgram(programHandle);
+                programHandle = 0;
+            }
+        }
 
-		if (programHandle == 0)
-		{
-			throw new RuntimeException("Error creating program.");
-		}
+        if (programHandle == 0) {
+            throw new RuntimeException("Error creating program.");
+        }
 
-		return programHandle;
-	}
+        return programHandle;
+    }
 }
