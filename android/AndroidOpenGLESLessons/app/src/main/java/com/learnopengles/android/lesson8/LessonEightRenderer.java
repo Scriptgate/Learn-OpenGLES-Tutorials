93a86b12d36356d2c4e921caa7755cbd5768ba9d
Scriptgate <wscheppers@hotmail.com>
2016-11-14 22:12:23 +0100

    Moved HeightMap into a separate class

--- android/AndroidOpenGLESLessons/app/src/main/java/com/learnopengles/android/lesson8/LessonEightRenderer.java
+++ android/AndroidOpenGLESLessons/app/src/main/java/com/learnopengles/android/lesson8/LessonEightRenderer.java
@@ -1,22 +1,15 @@
 package com.learnopengles.android.lesson8;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.FloatBuffer;
-import java.nio.ShortBuffer;
-
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
-
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
-import android.util.Log;
 
 import com.learnopengles.android.R;
 import com.learnopengles.android.common.RawResourceReader;
 import com.learnopengles.android.common.ShaderHelper;
-import com.learnopengles.android.lesson8.ErrorHandler.ErrorType;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
 
 /**
  * This class implements our custom renderer. Note that the GL10 parameter
@@ -24,12 +17,10 @@ import com.learnopengles.android.lesson8.ErrorHandler.ErrorType;
  * used instead.
  */
 public class LessonEightRenderer implements GLSurfaceView.Renderer {
-	/** Used for debug logs. */
-	private static final String TAG = "LessonEightRenderer";
 
 	/** References to other main objects. */
 	private final LessonEightActivity lessonEightActivity;
-	private final ErrorHandler errorHandler;
+
 
 	/**
 	 * Store the model matrix. This matrix is used to move models from object
@@ -68,30 +59,16 @@ public class LessonEightRenderer implements GLSurfaceView.Renderer {
 	private int mvMatrixUniform;
 	private int lightPosUniform;
 
-	/** OpenGL handles to our program attributes. */
-	private int positionAttribute;
-	private int normalAttribute;
-	private int colorAttribute;
-
 	/** Identifiers for our uniforms and attributes inside the shaders. */
 	private static final String MVP_MATRIX_UNIFORM = "u_MVPMatrix";
 	private static final String MV_MATRIX_UNIFORM = "u_MVMatrix";
 	private static final String LIGHT_POSITION_UNIFORM = "u_LightPos";
 
-	private static final String POSITION_ATTRIBUTE = "a_Position";
-	private static final String NORMAL_ATTRIBUTE = "a_Normal";
-	private static final String COLOR_ATTRIBUTE = "a_Color";
+	//TODO: These fields are used in both program and heightmap, find where they belong
+	public static final String POSITION_ATTRIBUTE = "a_Position";
+	public static final String NORMAL_ATTRIBUTE = "a_Normal";
+	public static final String COLOR_ATTRIBUTE = "a_Color";
 
-	/** Additional constants. */
-	private static final int POSITION_DATA_SIZE_IN_ELEMENTS = 3;
-	private static final int NORMAL_DATA_SIZE_IN_ELEMENTS = 3;
-	private static final int COLOR_DATA_SIZE_IN_ELEMENTS = 4;
-
-	private static final int BYTES_PER_FLOAT = 4;
-	private static final int BYTES_PER_SHORT = 2;
-
-	private static final int STRIDE = (POSITION_DATA_SIZE_IN_ELEMENTS + NORMAL_DATA_SIZE_IN_ELEMENTS + COLOR_DATA_SIZE_IN_ELEMENTS)
-			* BYTES_PER_FLOAT;
 
 	/**
 	 * Used to hold a light centered on the origin in model space. We need a 4th
@@ -129,12 +106,12 @@ public class LessonEightRenderer implements GLSurfaceView.Renderer {
 	 */
 	public LessonEightRenderer(final LessonEightActivity lessonEightActivity, ErrorHandler errorHandler) {
 		this.lessonEightActivity = lessonEightActivity;
-		this.errorHandler = errorHandler;
+		heightMap = new HeightMap(errorHandler);
 	}
 
 	@Override
 	public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
-		heightMap = new HeightMap();
+		heightMap.initialize();
 
 		// Set the background clear color to black.
 		GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -173,8 +150,7 @@ public class LessonEightRenderer implements GLSurfaceView.Renderer {
 		final int vertexShaderHandle = ShaderHelper.compileShader(GLES20.GL_VERTEX_SHADER, vertexShader);
 		final int fragmentShaderHandle = ShaderHelper.compileShader(GLES20.GL_FRAGMENT_SHADER, fragmentShader);
 
-		program = ShaderHelper.createAndLinkProgram(vertexShaderHandle, fragmentShaderHandle, new String[] {
-				POSITION_ATTRIBUTE, NORMAL_ATTRIBUTE, COLOR_ATTRIBUTE });
+		program = ShaderHelper.createAndLinkProgram(vertexShaderHandle, fragmentShaderHandle, new String[] {POSITION_ATTRIBUTE, NORMAL_ATTRIBUTE, COLOR_ATTRIBUTE });
 
 		// Initialize the accumulated rotation matrix
 		Matrix.setIdentityM(accumulatedRotation, 0);
@@ -209,9 +185,7 @@ public class LessonEightRenderer implements GLSurfaceView.Renderer {
 		mvpMatrixUniform = GLES20.glGetUniformLocation(program, MVP_MATRIX_UNIFORM);
 		mvMatrixUniform = GLES20.glGetUniformLocation(program, MV_MATRIX_UNIFORM);
 		lightPosUniform = GLES20.glGetUniformLocation(program, LIGHT_POSITION_UNIFORM);
-		positionAttribute = GLES20.glGetAttribLocation(program, POSITION_ATTRIBUTE);
-		normalAttribute = GLES20.glGetAttribLocation(program, NORMAL_ATTRIBUTE);
-		colorAttribute = GLES20.glGetAttribLocation(program, COLOR_ATTRIBUTE);
+
 
 		// Calculate position of the light. Push into the distance.
 		Matrix.setIdentityM(lightModelMatrix, 0);
@@ -262,173 +236,8 @@ public class LessonEightRenderer implements GLSurfaceView.Renderer {
 		GLES20.glUniform3f(lightPosUniform, lightPosInEyeSpace[0], lightPosInEyeSpace[1], lightPosInEyeSpace[2]);
 
 		// Render the heightmap.
-		heightMap.render();
+		heightMap.render(program);
 	}
 
-	class HeightMap {
-		static final int SIZE_PER_SIDE = 32;
-		static final float MIN_POSITION = -5f;
-		static final float POSITION_RANGE = 10f;
 
-		final int[] vbo = new int[1];
-		final int[] ibo = new int[1];
-
-		int indexCount;
-
-		HeightMap() {
-			try {
-				final int floatsPerVertex = POSITION_DATA_SIZE_IN_ELEMENTS + NORMAL_DATA_SIZE_IN_ELEMENTS
-						+ COLOR_DATA_SIZE_IN_ELEMENTS;
-				final int xLength = SIZE_PER_SIDE;
-				final int yLength = SIZE_PER_SIDE;
-
-				final float[] heightMapVertexData = new float[xLength * yLength * floatsPerVertex];
-
-				int offset = 0;
-
-				// First, build the data for the vertex buffer
-				for (int y = 0; y < yLength; y++) {
-					for (int x = 0; x < xLength; x++) {
-						final float xRatio = x / (float) (xLength - 1);
-						
-						// Build our heightmap from the top down, so that our triangles are counter-clockwise.
-						final float yRatio = 1f - (y / (float) (yLength - 1));
-						
-						final float xPosition = MIN_POSITION + (xRatio * POSITION_RANGE);
-						final float yPosition = MIN_POSITION + (yRatio * POSITION_RANGE);
-
-						// Position
-						heightMapVertexData[offset++] = xPosition;
-						heightMapVertexData[offset++] = yPosition;
-						heightMapVertexData[offset++] = ((xPosition * xPosition) + (yPosition * yPosition)) / 10f;					
-
-						// Cheap normal using a derivative of the function.
-						// The slope for X will be 2X, for Y will be 2Y.
-						// Divide by 10 since the position's Z is also divided by 10.
-						final float xSlope = (2 * xPosition) / 10f;
-						final float ySlope = (2 * yPosition) / 10f;
-						
-						// Calculate the normal using the cross product of the slopes.
-						final float[] planeVectorX = {1f, 0f, xSlope};
-						final float[] planeVectorY = {0f, 1f, ySlope};
-						final float[] normalVector = {
-								(planeVectorX[1] * planeVectorY[2]) - (planeVectorX[2] * planeVectorY[1]),
-								(planeVectorX[2] * planeVectorY[0]) - (planeVectorX[0] * planeVectorY[2]),
-								(planeVectorX[0] * planeVectorY[1]) - (planeVectorX[1] * planeVectorY[0])};
-						
-						// Normalize the normal						
-						final float length = Matrix.length(normalVector[0], normalVector[1], normalVector[2]);
-
-						heightMapVertexData[offset++] = normalVector[0] / length;
-						heightMapVertexData[offset++] = normalVector[1] / length;
-						heightMapVertexData[offset++] = normalVector[2] / length;
-
-						// Add some fancy colors.
-						heightMapVertexData[offset++] = xRatio;
-						heightMapVertexData[offset++] = yRatio;
-						heightMapVertexData[offset++] = 0.5f;
-						heightMapVertexData[offset++] = 1f;
-					}
-				}
-
-				// Now build the index data
-				final int numStripsRequired = yLength - 1;
-				final int numDegensRequired = 2 * (numStripsRequired - 1);
-				final int verticesPerStrip = 2 * xLength;
-
-				final short[] heightMapIndexData = new short[(verticesPerStrip * numStripsRequired) + numDegensRequired];
-
-				offset = 0;
-
-				for (int y = 0; y < yLength - 1; y++) {
-					if (y > 0) {
-						// Degenerate begin: repeat first vertex
-						heightMapIndexData[offset++] = (short) (y * yLength);
-					}
-
-					for (int x = 0; x < xLength; x++) {
-						// One part of the strip
-						heightMapIndexData[offset++] = (short) ((y * yLength) + x);
-						heightMapIndexData[offset++] = (short) (((y + 1) * yLength) + x);
-					}
-
-					if (y < yLength - 2) {
-						// Degenerate end: repeat last vertex
-						heightMapIndexData[offset++] = (short) (((y + 1) * yLength) + (xLength - 1));
-					}
-				}
-
-				indexCount = heightMapIndexData.length;
-
-				final FloatBuffer heightMapVertexDataBuffer = ByteBuffer
-						.allocateDirect(heightMapVertexData.length * BYTES_PER_FLOAT).order(ByteOrder.nativeOrder())
-						.asFloatBuffer();
-				heightMapVertexDataBuffer.put(heightMapVertexData).position(0);
-
-				final ShortBuffer heightMapIndexDataBuffer = ByteBuffer
-						.allocateDirect(heightMapIndexData.length * BYTES_PER_SHORT).order(ByteOrder.nativeOrder())
-						.asShortBuffer();
-				heightMapIndexDataBuffer.put(heightMapIndexData).position(0);
-
-				GLES20.glGenBuffers(1, vbo, 0);
-				GLES20.glGenBuffers(1, ibo, 0);
-
-				if (vbo[0] > 0 && ibo[0] > 0) {
-					GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vbo[0]);
-					GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, heightMapVertexDataBuffer.capacity() * BYTES_PER_FLOAT,
-							heightMapVertexDataBuffer, GLES20.GL_STATIC_DRAW);
-
-					GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, ibo[0]);
-					GLES20.glBufferData(GLES20.GL_ELEMENT_ARRAY_BUFFER, heightMapIndexDataBuffer.capacity()
-							* BYTES_PER_SHORT, heightMapIndexDataBuffer, GLES20.GL_STATIC_DRAW);
-
-					GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
-					GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, 0);
-				} else {
-					errorHandler.handleError(ErrorType.BUFFER_CREATION_ERROR, "glGenBuffers");
-				}
-			} catch (Throwable t) {
-				Log.w(TAG, t);
-				errorHandler.handleError(ErrorType.BUFFER_CREATION_ERROR, t.getLocalizedMessage());
-			}
-		}
-
-		void render() {
-			if (vbo[0] > 0 && ibo[0] > 0) {				
-				GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vbo[0]);
-
-				// Bind Attributes
-				GLES20.glVertexAttribPointer(positionAttribute, POSITION_DATA_SIZE_IN_ELEMENTS, GLES20.GL_FLOAT, false,
-						STRIDE, 0);
-				GLES20.glEnableVertexAttribArray(positionAttribute);
-
-				GLES20.glVertexAttribPointer(normalAttribute, NORMAL_DATA_SIZE_IN_ELEMENTS, GLES20.GL_FLOAT, false,
-						STRIDE, POSITION_DATA_SIZE_IN_ELEMENTS * BYTES_PER_FLOAT);
-				GLES20.glEnableVertexAttribArray(normalAttribute);
-
-				GLES20.glVertexAttribPointer(colorAttribute, COLOR_DATA_SIZE_IN_ELEMENTS, GLES20.GL_FLOAT, false,
-						STRIDE, (POSITION_DATA_SIZE_IN_ELEMENTS + NORMAL_DATA_SIZE_IN_ELEMENTS) * BYTES_PER_FLOAT);
-				GLES20.glEnableVertexAttribArray(colorAttribute);
-
-				// Draw
-				GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, ibo[0]);
-				GLES20.glDrawElements(GLES20.GL_TRIANGLE_STRIP, indexCount, GLES20.GL_UNSIGNED_SHORT, 0);
-
-				GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
-				GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, 0);
-			}
-		}
-
-		void release() {
-			if (vbo[0] > 0) {
-				GLES20.glDeleteBuffers(vbo.length, vbo, 0);
-				vbo[0] = 0;
-			}
-
-			if (ibo[0] > 0) {
-				GLES20.glDeleteBuffers(ibo.length, ibo, 0);
-				ibo[0] = 0;
-			}
-		}
-	}
 }
