29c15cdd3191b5239d8a995f6a14f228e368f435
Scriptgate <wscheppers@hotmail.com>
2017-05-04 21:49:32 +0200

    Extracted some methods and localized some fields

--- android/AndroidOpenGLESLessons/app/src/main/java/com/learnopengles/android/lesson8b/HeightMap.java
+++ android/AndroidOpenGLESLessons/app/src/main/java/com/learnopengles/android/lesson8b/HeightMap.java
@@ -31,16 +31,12 @@ public class HeightMap {
     static final float MIN_POSITION = -5f;
     static final float POSITION_RANGE = 10f;
 
-    /** OpenGL handles to our program attributes. */
-    private int positionAttribute;
-    private int colorAttribute;
-
     private final ErrorHandler errorHandler;
 
-    final int[] vbo = new int[1];
-    final int[] ibo = new int[1];
+    private final int[] vbo = new int[1];
+    private final int[] ibo = new int[1];
 
-    int indexCount;
+    private int indexCount;
 
     HeightMap(ErrorHandler errorHandler) {
         this.errorHandler = errorHandler;
@@ -49,71 +45,16 @@ public class HeightMap {
     public void initialize() {
         try {
             final int floatsPerVertex = POSITION_DATA_SIZE_IN_ELEMENTS + COLOR_DATA_SIZE_IN_ELEMENTS;
-            final int xLength = SIZE_PER_SIDE;
-            final int yLength = SIZE_PER_SIDE;
 
-            final float[] heightMapVertexData = new float[xLength * yLength * floatsPerVertex];
-
-            int offset = 0;
-
-            // First, build the data for the vertex buffer
-            for (int y = 0; y < yLength; y++) {
-                for (int x = 0; x < xLength; x++) {
-                    final float xRatio = x / (float) (xLength - 1);
-
-                    // Build our heightmap from the top down, so that our triangles are counter-clockwise.
-                    final float yRatio = 1f - (y / (float) (yLength - 1));
-
-                    final float xPosition = MIN_POSITION + (xRatio * POSITION_RANGE);
-                    final float yPosition = MIN_POSITION + (yRatio * POSITION_RANGE);
-
-                    // Position
-                    heightMapVertexData[offset++] = xPosition;
-                    heightMapVertexData[offset++] = yPosition;
-                    heightMapVertexData[offset++] = ((xPosition * xPosition) + (yPosition * yPosition)) / 10f;
-
-                    // Add some fancy colors.
-                    heightMapVertexData[offset++] = xRatio;
-                    heightMapVertexData[offset++] = yRatio;
-                    heightMapVertexData[offset++] = 0.5f;
-                    heightMapVertexData[offset++] = 1f;
-                }
-            }
-
-            // Now build the index data
-            final int numStripsRequired = yLength - 1;
-            final int numDegensRequired = 2 * (numStripsRequired - 1);
-            final int verticesPerStrip = 2 * xLength;
-
-            final short[] heightMapIndexData = new short[(verticesPerStrip * numStripsRequired) + numDegensRequired];
-
-            offset = 0;
-
-            for (int y = 0; y < yLength - 1; y++) {
-                if (y > 0) {
-                    // Degenerate begin: repeat first vertex
-                    heightMapIndexData[offset++] = (short) (y * yLength);
-                }
-
-                for (int x = 0; x < xLength; x++) {
-                    // One part of the strip
-                    heightMapIndexData[offset++] = (short) ((y * yLength) + x);
-                    heightMapIndexData[offset++] = (short) (((y + 1) * yLength) + x);
-                }
-
-                if (y < yLength - 2) {
-                    // Degenerate end: repeat last vertex
-                    heightMapIndexData[offset++] = (short) (((y + 1) * yLength) + (xLength - 1));
-                }
-            }
-
-            indexCount = heightMapIndexData.length;
-
+            final float[] heightMapVertexData = buildVertexData(floatsPerVertex, SIZE_PER_SIDE, SIZE_PER_SIDE);
             final FloatBuffer heightMapVertexDataBuffer = allocateDirect(heightMapVertexData.length * BYTES_PER_FLOAT).order(nativeOrder()).asFloatBuffer();
             heightMapVertexDataBuffer.put(heightMapVertexData).position(0);
 
+            final short[] heightMapIndexData = buildIndexData(SIZE_PER_SIDE, SIZE_PER_SIDE);
             final ShortBuffer heightMapIndexDataBuffer = allocateDirect(heightMapIndexData.length * BYTES_PER_SHORT).order(nativeOrder()).asShortBuffer();
             heightMapIndexDataBuffer.put(heightMapIndexData).position(0);
+
+            indexCount = heightMapIndexData.length;
 
             glGenBuffers(1, vbo, 0);
             glGenBuffers(1, ibo, 0);
@@ -136,22 +77,79 @@ public class HeightMap {
         }
     }
 
+    private short[] buildIndexData(int xLength, int yLength) {
+        // Now build the index data
+        final int numStripsRequired = yLength - 1;
+        final int numDegensRequired = 2 * (numStripsRequired - 1);
+        final int verticesPerStrip = 2 * xLength;
+
+        final short[] heightMapIndexData = new short[(verticesPerStrip * numStripsRequired) + numDegensRequired];
+
+        int offset = 0;
+
+        for (int y = 0; y < yLength - 1; y++) {
+            if (y > 0) {
+                // Degenerate begin: repeat first vertex
+                heightMapIndexData[offset++] = (short) (y * yLength);
+            }
+
+            for (int x = 0; x < xLength; x++) {
+                // One part of the strip
+                heightMapIndexData[offset++] = (short) ((y * yLength) + x);
+                heightMapIndexData[offset++] = (short) (((y + 1) * yLength) + x);
+            }
+
+            if (y < yLength - 2) {
+                // Degenerate end: repeat last vertex
+                heightMapIndexData[offset++] = (short) (((y + 1) * yLength) + (xLength - 1));
+            }
+        }
+        return heightMapIndexData;
+    }
+
+    private float[] buildVertexData(int floatsPerVertex, int xLength, int yLength) {
+        final float[] heightMapVertexData = new float[xLength * yLength * floatsPerVertex];
+
+        int offset = 0;
+
+        // First, build the data for the vertex buffer
+        for (int y = 0; y < yLength; y++) {
+            for (int x = 0; x < xLength; x++) {
+                final float xRatio = x / (float) (xLength - 1);
+
+                // Build our heightmap from the top down, so that our triangles are counter-clockwise.
+                final float yRatio = 1f - (y / (float) (yLength - 1));
+
+                final float xPosition = MIN_POSITION + (xRatio * POSITION_RANGE);
+                final float yPosition = MIN_POSITION + (yRatio * POSITION_RANGE);
+
+                // Position
+                heightMapVertexData[offset++] = xPosition;
+                heightMapVertexData[offset++] = yPosition;
+                heightMapVertexData[offset++] = ((xPosition * xPosition) + (yPosition * yPosition)) / 10f;
+
+                // Add some fancy colors.
+                heightMapVertexData[offset++] = xRatio;
+                heightMapVertexData[offset++] = yRatio;
+                heightMapVertexData[offset++] = 0.5f;
+                heightMapVertexData[offset++] = 1f;
+            }
+        }
+        return heightMapVertexData;
+    }
+
     void render(Program program) {
         if (vbo[0] > 0 && ibo[0] > 0) {
-
-            positionAttribute = program.getHandle(POSITION);
-            colorAttribute = program.getHandle(COLOR);
-
             glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 
-            // Bind Attributes
+            int positionAttribute = program.getHandle(POSITION);
             glVertexAttribPointer(positionAttribute, POSITION_DATA_SIZE_IN_ELEMENTS, GL_FLOAT, false, STRIDE, 0);
             glEnableVertexAttribArray(positionAttribute);
 
+            int colorAttribute = program.getHandle(COLOR);
             glVertexAttribPointer(colorAttribute, COLOR_DATA_SIZE_IN_ELEMENTS, GL_FLOAT, false, STRIDE, POSITION_DATA_SIZE_IN_ELEMENTS * BYTES_PER_FLOAT);
             glEnableVertexAttribArray(colorAttribute);
 
-            // Draw
             glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo[0]);
             glDrawElements(GL_TRIANGLE_STRIP, indexCount, GL_UNSIGNED_SHORT, 0);
 
